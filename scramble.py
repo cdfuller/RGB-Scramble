c82030cf2bd6c7bb43ac2a1138eb3763153ac0e4
Cody Fuller <git@codyfuller.com>
2017-04-28 15:52:54 -0500

    Break into functions

--- scramble.py
+++ scramble.py
@@ -1,22 +1,55 @@
 from PIL import Image
+from collections import deque
+import cProfile
 
-NUM_COLORS = 64
+IMG_WIDTH = 4096
+IMG_HEIGHT = 4096
 
-colors = []
+image = Image.new("RGB", (IMG_WIDTH, IMG_HEIGHT))
 
-for r in range(0, 64):
-  for g in range(0, 64):
-    for b in range(0, 64):
-      colors.append((r*4, g*4, b*4))
 
-im = Image.new("RGB", (512, 512))
+def generate_color_array():
+  colors = []
+  print("Generating colors")
+  for r in range(0, 256):
+    for g in range(0, 256):
+      for b in range(0, 256):
+        colors.append((r, g, b))
+  return colors
 
-px = im.load()
+def insert_colors(colors, image):
+  px = image.load()
+  counter = 0
+  total = IMG_WIDTH * IMG_HEIGHT
+  percentage_pt = int(total / 100 / 10) 
+  colors = deque(colors)
 
-for x in range(0, 512):
-  for y in range(0, 512):
-    px[x, y] = colors.pop()
+  print("Inserting colors")
 
-# im.save("00-HELLO-RGB.png")
+  for x in range(0, IMG_WIDTH):
+    for y in range(0, IMG_HEIGHT):
+      counter += 1
+      # print(counter, counter / total)
+      if counter % percentage_pt == 0:
+        print("{}%".format(counter / total * 100))
+      
+      px[x, y] = colors.pop()
 
-im.show()
+      # if counter % 2 == 0:
+      #   px[x, y] = colors.pop()
+      # else:
+      #   px[x, y] = colors.popleft()
+
+  print("100%")
+  # return image
+# im.save("03-HELLO-24-BIT.png")
+
+colors = generate_color_array()
+# cProfile.run("insert_colors(colors, image)")
+insert_colors(colors, image)
+
+image.show()
+
+
+# def sort_by_hue(color1, color2):
+
